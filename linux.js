45e1e1e5ee8f44bd5e9e8dfa160a38ca9b609365
Vitaliy Ribachenko <vit@ribachenko.com>
2017-09-28 12:05:07 +0300

    Fix generation OS X app when not providing icon. Fix error handling

--- linux.js
+++ linux.js
@@ -2,7 +2,7 @@
 const sh = require('tag-shell');
 
 module.exports = opts => {
-  sh.async`notify-send -a ${opts.app} -i ${opts.icon}
+  return sh.async`notify-send -a ${opts.app} -i ${opts.icon}
     ${opts.title} ${opts.message}
-  `;
+  `.on('error', () => {});
 };
--- windows/index.js
+++ windows/index.js
@@ -4,8 +4,8 @@ const sh = require('tag-shell');
 const notify = `${__dirname}/notify.ps1`;
 
 module.exports = opts => {
-  sh.async`powershell -file ${notify}
+  return sh.async`powershell -file ${notify}
     -app ${opts.app} -icon ${opts.icon}
     -title ${opts.title} -message ${opts.message}
-  `;
+  `.on('error', () => {});
 };
